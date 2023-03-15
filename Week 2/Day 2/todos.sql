60457144cc1d5f440578043c0b6791c6a39f7e81
Alfredo08 <alfredosv08@gmail.com>
2023-03-15 11:05:24 -0600

    Connecting to DB

--- Week 2/Day 2/todos.sql
+++ Week 2/Day 2/todos.sql
@@ -38,6 +38,39 @@ WHERE id = 5;
 SELECT *
 FROM users;
 
+INSERT INTO todos( name, status, user_id )
+VALUES ( 'Learning Flask', 'complete', 1 ),
+	   ( 'Learning Routes', 'complete', 1 ),
+       ( 'Learning Sessions', 'complete', 2 ),
+       ( 'Learning POST', 'complete', 3 ),
+       ( 'Learning SQL', 'in_progress', 1 ),
+       ( 'Learning ERD', 'in_progress', 2 );
+       
+INSERT INTO todos( name, status, user_id )
+VALUES ( 12, 20, 1 ) ;   
+
+DELETE FROM todos
+WHERE id = 8; 
+
+SELECT u.first_name, u.last_name, t.name, t.status
+FROM todos t 
+	JOIN users u ON t.user_id = u.id
+WHERE u.id = 1;
+
+SELECT *
+FROM todos t, users u
+WHERE t.user_id = u.id AND u.id = 1;
+
+SELECT u.first_name, u.last_name, t.name, t.status
+FROM users u
+	LEFT JOIN todos t ON u.id = t.user_id;
+
+
+SELECT u.first_name, COUNT( t.user_id ) AS 'Num of todos'
+FROM users u
+	JOIN todos t ON u.id = t.user_id
+GROUP BY ( u.first_name )
+ORDER BY DESC( u.first_name );
 
 
       
