8db5c5f010766f875d1d7af2236a1f5f8d0adbeb
Alfredo08 <alfredosv08@gmail.com>
2023-03-17 11:16:23 -0600

    Registration and flash validations

---
+++ Week 2/Day 5/Todos_App/flask_app/models/users_model.py
@@ -0,0 +1,57 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app import DATABASE
+from flask_app.models import todos_model
+from flask import flash
+
+class User:
+    def __init__( self, data ):
+        self.id = data["id"]
+        self.first_name = data["first_name"]
+        self.last_name = data["last_name"]
+        self.created_at = data["created_at"]
+        self.updated_at = data["updated_at"]
+        self.email = data["email"]
+        self.password = data["password"]
+        self.todo_list = []
+    
+    @classmethod
+    def get_one_with_todos( cls, data ):
+        query  = "SELECT * "
+        query += "FROM users u "
+        query += "LEFT JOIN todos t ON u.id = t.user_id "
+        query += "WHERE u.id = %(id)s;"
+
+        results = connectToMySQL( DATABASE ).query_db( query, data )
+        current_user = cls( results[ 0 ] )
+
+        for row in results:
+            if row[ "name" ] != None:
+                current_todo = {
+                    "id" : row[ "t.id" ],
+                    "name" : row[ "name" ],
+                    "status" : row[ "status" ],
+                    "created_at" : row[ "t.created_at" ],
+                    "updated_at" : row[ "t.updated_at" ],
+                    "user_id" : row[ "user_id" ]
+                }
+                current_todo_object = todos_model.Todo( current_todo )
+                current_user.todo_list.append( current_todo_object )
+        print( "Todos", current_user.todo_list )
+        return current_user
+    
+    @classmethod
+    def create_one( cls, data ):
+        query  = "INSERT INTO users( email, password, first_name, last_name ) "
+        query += "VALUES( %(email)s, %(password)s, %(first_name)s, %(last_name)s ); "
+
+        result = connectToMySQL( DATABASE ).query_db( query, data )
+        return result
+
+    @staticmethod
+    def validate_registration( data ):
+        is_valid = True
+        if len( data['first_name'] ) == 0:
+            flash( "You must provide your name!", "first_name_error" )
+            is_valid = False
+
+        return is_valid
