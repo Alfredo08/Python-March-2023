3e4fff57479ceae1d9d84069335225c5e4b51e08
Alfredo08 <alfredosv08@gmail.com>
2023-03-21 17:47:37 -0600

    Adding Algos Day 2 Week 3

---
+++ Week 3/Day 2/Algos/balanceIndex.js
@@ -0,0 +1,30 @@
+/* 
+  Balance Index
+  Here, a balance point is ON an index, not between indices.
+  Return the balance index where sums are equal on either side
+  (exclude its own value).
+  
+  Return -1 if none exist.
+  
+*/
+
+const nums1 = [-2, 5, 7, 0, 3];
+const expected1 = 2;
+
+const nums2 = [1, 2, 3, 4, 5, 2, 6, 6, 3]
+const expected2 = 5;
+
+const nums3 = [9, 9];
+const expected3 = -1;
+
+const nums4 = [1, 2, 3, 4];
+const expected4 = -1;
+/**
+ * Finds the balance index in the given array where the sum to the left of the
+ *    index is equal to the sum to the right of the index.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums
+ * @returns {number} The balance index or -1 if there is none.
+ */
+function balanceIndex(nums) {}
