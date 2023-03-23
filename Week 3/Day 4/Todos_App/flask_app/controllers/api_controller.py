a35ef5a26f1ed9b9b1109624047f20414fb10549
Alfredo08 <alfredosv08@gmail.com>
2023-03-23 14:57:03 -0600

    Finishing API demonstration

--- Week 3/Day 4/Todos_App/flask_app/controllers/api_controller.py
+++ Week 3/Day 4/Todos_App/flask_app/controllers/api_controller.py
@@ -9,3 +9,31 @@ from flask_cors import cross_origin
 def api_get_all():
     list_of_todos = Todo.api_get_all()
     return ( jsonify( list_of_todos ) , 200 )
+
+@app.route( '/api/delete/todo/<int:id>', methods = ['DELETE'] )
+@cross_origin( origins = ["http://127.0.0.1:5500"] )
+def api_delete_one( id ):
+    data = {
+        "todo_id" : id
+    }
+    message = Todo.delete_one( data )
+    return ( ( jsonify({}) ), 204 )
+
+@app.route( '/api/new/todo', methods = ['POST'] )
+@cross_origin( origins = ["http://127.0.0.1:5500"] )
+def api_add_one():
+    new_todo = json.loads( request.data.decode( 'UTF-8' ) )
+    todo_id = Todo.create_one( new_todo )
+    response_dictionary = {
+        "message" : "Todo added.",
+        "todo_id" : todo_id 
+    }
+    return ( jsonify( response_dictionary ) ), 201
+
+@app.route( '/api/update/todo/<int:id>', methods = ['PUT'] )
+@cross_origin( origins = ["http://127.0.0.1:5500"] )
+def api_update_one( id ):
+    todo_update = json.loads( request.data.decode( 'UTF-8' ) )
+    todo_update[ "todo_id" ] = id
+    Todo.update_one( todo_update )
+    return ( jsonify( { "message" : "Todo has been updated" } ) ), 202
--- Week 3/Day 4/Todos_Client/js/index.js
+++ Week 3/Day 4/Todos_Client/js/index.js
@@ -1,17 +1,23 @@
 
 async function loadTodos( element ){
     let url = "http://127.0.0.1:5001/api/todos";
-    let response = await fetch( url );
-    console.log( response );
+
+    let settings = {
+        method : "GET"
+    };
+
+    let response = await fetch( url, settings );
     let data = await response.json();
-    console.log( data );
 
     let results_Div = document.querySelector( ".results" );
     results_Div.innerHTML = "";
     for( let i = 0; i < data.length; i ++ ){
         results_Div.innerHTML += 
-        `<p>
-            ${data[i].name} - ${data[i].status}
+        `<p id="todo_${data[i].id}">
+            ${data[i].name} - ${data[i].status} 
+            <button onclick="deleteTodo(${data[i].id})">
+                Delete
+            </button>
          </p>
         `;
     }
@@ -33,5 +39,41 @@ async function loadTodos( element ){
         `;
     }
     */
+
+}
+
+async function deleteTodo( todoId ){
+    let url = `http://127.0.0.1:5001/api/delete/todo/${todoId}`;
+    let settings = {
+        method : "DELETE"
+    };
+
+    let response = await fetch( url, settings );
+    console.log( response );
+
+    document.querySelector( `#todo_${todoId}` ).remove();
+}
+
+async function addTodo( event ){
+    event.preventDefault();
+    let url = "http://127.0.0.1:5001/api/new/todo";
+    
+    let data = {
+        name : document.querySelector( '#todo_name' ).value,
+        status : document.querySelector( '#todo_status' ).value,
+        user_id : 6
+    };
+
+    let settings = {
+        method : "POST",
+        headers : {
+            "Content-type" : "application/json"
+        },
+        body : JSON.stringify( data )
+    }
+
+    let response = await fetch( url, settings );
+    let responseData = await response.json();
+    console.log( responseData );
 
 }
